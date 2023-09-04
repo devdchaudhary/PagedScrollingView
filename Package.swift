da6072bc845656a12bc551a636ceb34444049542
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-09-04 19:45:53 +0530

    name change

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI-PagedScrollingView",
+    name: "PagedScrollingView",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUI-PagedScrollingView",
+            name: "PagedScrollingView",
             targets: ["SwiftUI-PagedScrollingView"]),
     ],
     dependencies: [
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUI-PagedScrollingView",
+            name: "PagedScrollingView",
             dependencies: [],
             resources: [.process("Resources")]
         )
